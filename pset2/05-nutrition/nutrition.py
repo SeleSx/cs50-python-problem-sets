21c9324a546ef3ce4bed18a38578fae66afa86ec
SeleSx <saffet.sinanoglu@hotmail.com>
2023-11-28 21:23:20 +0300

    pset2 done

---
+++ pset2/05-nutrition/nutrition.py
@@ -0,0 +1,46 @@
+""" def makeDict():
+    dict = {}
+    i = 0
+    while i < 20:
+        name = input("name: ")
+        cal = int(input("cal: "))
+
+        dict[name] = cal
+        i += 1
+    
+    print("{")
+    for k in dict:
+        print("\t\"" + k + "\":" + str(dict[k]) + ",\n", end ="")
+    print("}")
+
+makeDict() """
+
+
+fruits = {
+    "apple":130,
+    "avocado":50,
+    "banana":110,
+    "cantaloupe":50,
+    "grapefruit":60,
+    "grapes":90,
+    "honeydew melon":50,
+    "kiwifruit":90,
+    "lemon":15,
+    "lime":20,
+    "nectarine":60,
+    "orange":80,
+    "peach":60,
+    "pear":100,
+    "pineapple":50,
+    "plums":70,
+    "strawberries":50,
+    "sweet cherries":100,
+    "tangerine":50,
+    "watermelon":80
+}
+
+fruit = input("Item: ").lower()
+if fruit not in fruits:
+    pass
+else:
+    print("Calories: ", fruits[fruit])
